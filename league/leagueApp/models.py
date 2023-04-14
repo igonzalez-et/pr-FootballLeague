3187e206fd33082553f0a91ce056bfc7584cb826
igonzalez-et <igonzalezvirsa@gmail.com>
2023-04-14 17:07:54 +0200

    Falta crear partidos

--- league/leagueApp/models.py
+++ league/leagueApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class League(models.Model):
     name = models.CharField(max_length=100)
+    # teams = models.ManyToManyField(Team)
 
     def __str__(self):
         return self.name
--- league/leagueApp/urls.py
+++ league/leagueApp/urls.py
@@ -5,9 +5,11 @@ app_name = 'leagueApp'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('clasificacion/', views.standings, name='clasificacion'),
+    path('clasificacion/<int:league_id>/', views.standings, name='clasificacion'),
     path('goleadores/', views.top_scorers, name='goleadores'),
     path('resultados/', views.results, name='resultados'),
     path('menu/', views.menu, name='menu'),
     path('crear_liga/', views.crear_liga, name='crear_liga'),
+    path('crear_partido/', views.crear_partido, name='crear_partido'),
+    path('crear_partido/<int:league_id>', views.escoger_equipos_partido, name='escoger_equipos_partido'),
 ]
